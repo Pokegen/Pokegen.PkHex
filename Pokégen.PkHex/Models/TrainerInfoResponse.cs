05371c185a24f441bd556c295128093fd0f2427c
Yukine <devyukine@gmx.de>
2021-11-28 23:47:40 +0100

    docs(TrainerInfoResponse): document trainer info response

--- Pokégen.PkHex/Models/TrainerInfoResponse.cs
+++ Pokégen.PkHex/Models/TrainerInfoResponse.cs
@@ -1,3 +1,13 @@
 namespace Pokégen.PkHex.Models;
 
-public record TrainerInfoResponse(int trainerId, int secretId, string OT, int gender, int game, int language, int generation);
+/// <summary>
+/// API payload containing trainer info
+/// </summary>
+/// <param name="TrainerId">Id of the trainer</param>
+/// <param name="SecretId">Secret Id of trainer</param>
+/// <param name="Ot">OT name of trainer</param>
+/// <param name="Gender">Gender of trainer</param>
+/// <param name="Game">Game of the trainer</param>
+/// <param name="Language">Language of the trainer</param>
+/// <param name="Generation">Generation of the trainer</param>
+public record TrainerInfoResponse(int TrainerId, int SecretId, string Ot, int Gender, int Game, int Language, int Generation);
