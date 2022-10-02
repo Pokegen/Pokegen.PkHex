8bbfe4b69e617e3ab0569adf92f1925db798dd6f
Yukine <devyukine@gmx.de>
2022-10-02 07:44:28 +0200

    style: move expression body to new line

--- Pokégen.PkHex/Extensions/PKMExtensions.cs
+++ Pokégen.PkHex/Extensions/PKMExtensions.cs
@@ -8,5 +8,6 @@ public static class PkmExtensions
 	public static bool IsLegal(this PKM pkm) 
 		=> new LegalityAnalysis(pkm).Valid && pkm.CanBeTraded();
 	
-	private static bool CanBeTraded(this PKM pkm) => !FormInfo.IsFusedForm(pkm.Species, pkm.Form, pkm.Format);
+	private static bool CanBeTraded(this PKM pkm) 
+		=> !FormInfo.IsFusedForm(pkm.Species, pkm.Form, pkm.Format);
 }
